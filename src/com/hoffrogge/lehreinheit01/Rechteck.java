4243164a3d10810bc64fc7487b8c1aa5d339f446
Kurs <Kurs@172.16.99.121>
2019-03-07 14:13:33 +0100

    Rechtecke aus einzelnen Linien zeichnen

---
+++ src/com/hoffrogge/lehreinheit01/Rechteck.java
@@ -0,0 +1,52 @@
+package com.hoffrogge.lehreinheit01;
+
+import java.awt.Graphics;
+
+public class Rechteck {
+
+	private int xKooridnate;
+	private int yKoordinate;
+
+	public Rechteck(int xKooridnate, int yKoordinate) {
+		super();
+		this.xKooridnate = xKooridnate;
+		this.yKoordinate = yKoordinate;
+	}
+
+	public void zeichnen(Graphics graphics) {
+
+		graphics.drawLine(xKooridnate, yKoordinate, xKooridnate + 100, yKoordinate);
+		graphics.drawLine(xKooridnate, yKoordinate, xKooridnate, yKoordinate + 100);
+		graphics.drawLine(xKooridnate + 100, yKoordinate, xKooridnate + 100, yKoordinate + 100);
+		graphics.drawLine(xKooridnate, yKoordinate + 100, xKooridnate + 100, yKoordinate + 100);
+	}
+}
+/*
+ * Draws a line, using the current color, between the points (x1, y1) and (x2,
+ * y2) in this graphics context's coordinate system.
+ */
+//
+// int x1 = 150;
+// int y1 = 600;
+// int x2 = 150;
+// int y2 = 300;
+// graphics.drawLine(x1, y1, x2, y2);
+// int x3 = 650;
+// int y3 = 600;
+// int x4 = 650;
+// int y4 = 300;
+// public Rechteck(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int
+// y4)
+// graphics.drawLine(x3, y3, x4, y4);{
+// super();
+// this.x1 = x1;
+// this.y1 = y1;
+// this.x2 = x2;
+// this.y2 = y2;
+// this.x3 = x3;
+// this.y3 = y3;
+// this.x4 = x4;
+// this.y4 = y4;
+//
+// }
+// }
